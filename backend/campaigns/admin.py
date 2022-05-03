5cb248611ed3f7454557b9e6c74117561d744730
ritavdas <ritavdas@gmail.com>
2022-05-03 23:32:54 +0530

    We can modify how the model will look in the Django Admin using the admin.ModelAdmin Added pagination using list_per_page feature

--- backend/campaigns/admin.py
+++ backend/campaigns/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register([Campaign,Subscriber])
+
+# describing how our model will look in the Django-Admin
+class CampaignModelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at')
+    search_fields = ('title', 'description')
+    list_per_page = 1 
+
+
+admin.site.register(Campaign, CampaignModelAdmin)
+admin.site.register(Subscriber)
