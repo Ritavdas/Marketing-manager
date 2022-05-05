8162e57cddc6ba67cdcd22ef47d64f8fc507f84b
ritavdas <ritavdas@gmail.com>
2022-05-05 12:48:42 +0530

    GetStaticProps only shows console logs in the server environment (host pc) In web applications, there is the client and the server. The client is a web browser, like Internet Explorer, Google Chrome, Firefox, etc. The server is a web application server at a remote location that will process web requests and send pages to the client. Made a try catch block to check if the error is due to server issues or something else Load environment variable at env.local Functions like getstaticprops work on server while hooks like useEffect work on browser. Environment variable only accesible on the server and not on the browser/client. If we want something to be accessible to browser from env.local. we have to prefix it with NEXT_PUBLIC

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,18 +1,26 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect } from "react";
 import styles from "../styles/Home.module.css";
 
-export default function Home({ data, done }) {
+export default function Home({ data, done, error }) {
+  useEffect(() => {
+    console.log(
+      "process.env.NEXT_PUBLIC_BASE_URL :",
+      process.env.NEXT_PUBLIC_BASE_URL
+    );
+  }, []);
   console.log("data : ", data);
   return (
     <div>
       <main>
         <h1>Available Campaigns</h1>
+        {error && <p> {JSON.stringify(error)} </p>}
         {data.map((elem) => (
           <div key={elem.slug}>
             <div>
               <div>
-                <img
+                <Image
                   src={"https://res.cloudinary.com/dnre36mya/" + elem.logo}
                   height={120}
                   width={120}
@@ -33,15 +41,26 @@ export default function Home({ data, done }) {
 }
 
 export async function getStaticProps() {
-  // For fetching data in real time
-  const response = await fetch("http://localhost:8000/api/campaigns");
+  let data = [];
+  let error = null;
 
-  // The actual data in a promise so we convert it to JSON
-  const data = await response.json();
+  try {
+    // For fetching data in real time
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/campaigns`
+    );
+
+    // The actual data in a promise so we convert it to JSON
+    data = await response.json();
+  } catch (err) {
+    console.log("err >>", err);
+    error = err.message ? err.message : "Something went wrong";
+  }
 
   return {
     props: {
       data,
+      error,
       done: true,
       // These props will be now available in the element
     },
