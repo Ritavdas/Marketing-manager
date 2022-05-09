f80594bf5ab472ccbccc6871ff1ce578738d98a8
ritavdas <ritavdas@gmail.com>
2022-05-09 17:26:15 +0530

    CSS Modules Landing Page design Z-index : Enables us to position stuff on top of each other.

--- frontend/src/pages/[slug].js
+++ frontend/src/pages/[slug].js
@@ -1,42 +1,89 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
+import Head from "next/head";
 import Link from "next/link";
-import styles from "../styles/Home.module.css";
+// import styles from "../styles/Home.module.css";
+import styles from "../styles/Detail.module.css";
 import dateformat from "dateformat";
 
 function Campaign({ data }) {
   console.log("data : ", data);
 
-  const {query: { slug },} = useRouter();
+  const {
+    query: { slug },
+  } = useRouter();
 
   console.log("router: ", slug);
   return (
-    <div className={styles.main}>
-      <div key={data.slug}>
-        <div className={styles.item}>
-          <div className={styles.imgFormat}>
-            <Image
-              className={styles.img}
-              src={"https://res.cloudinary.com/dnre36mya/" + data.logo}
-              height={120}
-              width={120}
-              alt="Campaign banner"
-            />
-          </div>
-          <div className={styles.rightItems}>
-            <Link href={"/" + data.slug}>
-              <a>{data.title}</a>
-            </Link>
-            <p>{data.description}</p>
-            <small>
-              {dateformat(
-                new Date(data.created_at),
-                "dddd, mmmm dS, yyyy, h:MM:ss TT"
-              )}
-            </small>
+    <div>
+      <Head>
+        <title>{data.title}</title>
+        <meta name="description" content={data.description} />
+      </Head>
+
+      <div className={styles.wrapper}>
+        <div className={styles.main}></div>
+
+        <div className={styles.contents}>
+          <Image
+            className={styles.img}
+            src={"https://res.cloudinary.com/dnre36mya/" + data.logo}
+            height={120}
+            width={120}
+            alt="Campaign banner"
+          />
+          <div className={styles.grid}>
+            <div className={styles.left}>
+              <h1 className={styles.title}>{data.title}</h1>
+              <p className={styles.description}>{data.description}</p>
+            </div>
+            <div className={styles.right}>
+              <div className={styles.rightContents}>
+                <form>
+                  <div className={styles.formGroup}>
+                    <input
+                      type="email"
+                      required
+                      name="email"
+                      placeholder="Email address"
+                      className={styles.input}
+                    />
+                  </div>
+                  <div className={styles.submit}>
+                    <input className={styles.button} type="submit" />
+                    <p className={styles.consent}>
+                      We respect your privacy. Unsubscribe at any time.
+                    </p>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
+
+        <div key={data.slug}>
+          <div className={styles.item}>
+            <div className={styles.imgFormat}></div>
+            <div className={styles.rightItems}>
+              <Link href={"/" + data.slug}>
+                <a>{data.title}</a>
+              </Link>
+              <p>{data.description}</p>
+              <small>
+                {dateformat(
+                  new Date(data.created_at),
+                  "dddd, mmmm dS, yyyy, h:MM:ss TT"
+                )}
+              </small>
+            </div>
+          </div>
+        </div>
+        <footer className={styles.footer}>
+          <Link href="/">
+            <a>Go back to List</a>
+          </Link>
+        </footer>
       </div>
     </div>
   );
